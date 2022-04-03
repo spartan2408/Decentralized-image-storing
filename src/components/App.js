24369babbc5b26b765ea733d8f196b8efb1f87c2
spartan2408 <amoghkrish1@gmail.com>
2022-04-03 20:02:45 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,92 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import Web3 from 'web3';
 import './App.css';
+import Meme from '../abis/Meme.json'
+
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: "ipfs.infura.io", port: '5001', protocol: 'https' })
 
 class App extends Component {
+
+  async componentWillMount(){
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+
+  async loadBlockchainData(){
+    const web3= window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0]})
+    const networkId = await web3.eth.net.getId()
+    const networkData = Meme.networks[networkId]
+    if(networkData){
+      const abi = Meme.abi
+      const address = networkData.address
+      //fetch contract
+      const contract = web3.eth.Contract(abi,address)
+      this.setState({ contract})
+      const memeHash = await contract.methods.get().call()
+      this.setState({memeHash})
+    }else{
+      window.alert('smart contract not deployed to detected network')
+    }
+
+  }
+  constructor(props) {
+    super(props);
+    this.state ={
+      account: '',
+      buffer : null,
+      contract: null,
+      memeHash:'QmNYZpu7FJfK1YVQfiT4MJ8aHEaKkJCGyyXa7SUH3uXASp'
+  };
+}
+
+
+async loadWeb3(){
+  if(window.ethereum){
+    window.web3 = new Web3(window.ethereum)
+    await window.ethereum.enable()
+  }if(window.web3){
+    window.web3 = new Web3(window.web3.currentProvider)
+
+  }else{
+     window.alert('Please use MetMask!')
+  }
+}
+
+ captureFile =(event) =>{
+    event.preventDefault()
+      //process file for IPFS...
+      const file= event.target.files[0]
+      const reader =new window.FileReader()
+      reader.readAsArrayBuffer(file)
+      reader.onloadend = () => {
+        this.setState({buffer: Buffer(reader.result)})
+      }
+  
+  }
+
+
+  onSubmit = (event) =>{
+    event.preventDefault()
+    console.log("Submitting the form...")
+    ipfs.add(this.state.buffer, (error, result) =>{
+      console.log('Ipfs result', result)
+      const memeHash = result[0].hash
+      if(error){ 
+        console.error(error)
+        return
+      }
+      this.state. contract.methods.set(memeHash).send({ from: this.state.account}).then((r) =>{
+              this.setState({ memeHash: memeHash})
+
+      })
+
+    })
+  }
+
   render() {
     return (
       <div>
@@ -13,32 +97,31 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            IMAGE of the day
           </a>
-        </nav>
+          <ul className ="navbar-nav px-3">
+            <li className="nav-item text nowrap d-none d-sm-none d-sm-block">
+              <small className="text-white">{this.state.account}
+              </small>
+            </li>
+          </ul>
+        </nav> 
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
                 <a
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="https://www.pes.edu/"
                   target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                  rel="noopener noreferrer">
+                  <img src= {`https://ipfs.infura.io/ipfs/${this.state.memeHash}`} />
+                  </a>
+                <p>&nbsp;</p>
+                <h2>Change IMAGE</h2>
+                <form onSubmit={this.onSubmit}>
+                 <input type= 'file' onChange={this.captureFile}/>
+                 <input type ='submit' />
+                </form>
               </div>
             </main>
           </div>
